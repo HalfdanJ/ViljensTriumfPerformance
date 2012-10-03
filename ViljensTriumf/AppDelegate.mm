fb11651ead82a07166a580e1235ff1ca6781ff9d
Jonas / Viljens Triumf <jonas@halfdanj.dk>
2012-10-03 12:12:00 +0200

    Initial commit

---
+++ ViljensTriumf/AppDelegate.mm
@@ -0,0 +1,42 @@
+//
+//  AppDelegate.m
+//  DEW
+//
+//  Created by Admin on 26/03/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+#import "Test.h"
+
+@implementation AppDelegate
+
+@synthesize window;
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    ocp = [[ofxCocoaPlugins alloc] initWithAppDelegate:self];
+    [ocp setNumberOutputviews:2];
+    [ocp addHeader:@"Setup"];
+    [ocp addPlugin:[[Keystoner alloc] initWithSurfaces:[NSArray arrayWithObjects:@"Floor", @"Wall", nil]] midiChannel:1];
+    [ocp addPlugin:[[Cameras alloc] initWithNumberCameras:1] midiChannel:1];
+    [ocp addPlugin:[[CameraCalibration alloc] init] midiChannel:1];
+    [ocp addPlugin:[[BlobTracker2d alloc] init] midiChannel:2];
+    [ocp addPlugin:[[Tracker alloc] init] midiChannel:2];
+    [ocp addPlugin:[[Midi alloc] init] midiChannel:2];
+//    [ocp addPlugin:[[OSCControl alloc] init] midiChannel:3];
+    
+    [ocp addHeader:@"DEW"];
+    [ocp addPlugin:[[Test alloc] init] midiChannel:12];
+
+    [ocp loadPlugins];
+
+}
+
+@end
